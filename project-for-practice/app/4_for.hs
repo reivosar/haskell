45d227e56ce11416fae9020cbc9ef151e5493cab
reivosar <nextcareerplus@gmail.com>
2020-11-21 20:54:54 +0900

    再起使わなくてもできる模様

--- project-for-practice/app/4_for.hs
+++ project-for-practice/app/4_for.hs
@@ -2,17 +2,10 @@ module Main where
 
 import Lib
 
---Haskellでは繰り返し構文が存在しないが
---以下のようにに再帰的に処理を行うことで
---繰り返し処理を実行できる
-for min max
-    | min == max = do
-        return max
-    | otherwise = do
-        print min
-        return =<< for (min + 1) max
-
 main :: IO ()
 main = do
-     print =<< for 1 10
-
+    let loop i | i < 5 = do
+            print i
+            loop $ i + 1
+        loop _ = return ()
+    loop 0
