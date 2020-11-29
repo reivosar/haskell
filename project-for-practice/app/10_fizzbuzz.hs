517cb33cf2417015d0491c23c1f35271aed013ea
reivosar <nextcareerplus@gmail.com>
2020-11-30 04:02:23 +0900

    リファクタリング

--- project-for-practice/app/10_fizzbuzz.hs
+++ project-for-practice/app/10_fizzbuzz.hs
@@ -2,15 +2,12 @@ module Main where
 
 import Lib
 
-fizzbuzz :: [Int] -> [String]
-fizzbuzz xs = map (calc) xs
-
-calc :: Int -> String
-calc i 
+fizzbuzz :: Int -> String
+fizzbuzz i 
     | (mod i 15) == 0 = "FizzBuzz"
     | (mod i 3)  == 0 = "Fizz"
     | (mod i 5)  == 0 = "Buzz"
     | otherwise       = (show i)
 
 main :: IO ()
-main = print $ fizzbuzz [1..100]
+main = print $ map (fizzbuzz) [1..100]
