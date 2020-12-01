b27a80071fd0862e6d181dab59e0ba61b3e9cd18
reivosar <nextcareerplus@gmail.com>
2020-12-02 05:09:36 +0900

    問題3追加。余裕

--- project-for-practice/app/11_codewars.hs
+++ project-for-practice/app/11_codewars.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Lib
+import Data.Char
 
 -- codewarsの問題解答
 -- Link https://www.codewars.com/
@@ -21,6 +22,16 @@ divisors a
     | otherwise = Right result
     where
         result = filter (\n -> (mod a n == 0)) [2..(a - 1)]
+
+-- ■ 問題3
+-- 文字列に同じ量の 'x' と 'o' があるかどうかをチェックします。このメソッドはブール値を返し、
+-- 大文字小文字を区別しません。文字列は任意の char を含むことができます。
+xo :: String -> Bool
+xo str = x_count == o_count
+    where
+        count char = length . filter ((==) char . toLower)
+        x_count = count 'x' str
+        o_count = count 'o' str
 
 main :: IO()
 main = print 1
