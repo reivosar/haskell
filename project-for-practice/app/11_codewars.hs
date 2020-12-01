73db5349cd91de4d533d3ab1be2fcbb3edcc586a
reivosar <nextcareerplus@gmail.com>
2020-12-02 03:08:41 +0900

    新規追加

---
+++ project-for-practice/app/11_codewars.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Lib
+
+-- codewarsの問題解答
+-- Link https://www.codewars.com/
+
+-- 10以下の自然数のうち、3または5の倍数であるものをすべて列挙すると、3, 5, 6, 9が得られる。
+-- これらの倍数の和は23である．
+-- 与えられた数の下にある3または5のすべての倍数の和を返すように解を完成させてください．
+solution :: Integer -> Integer
+solution number = sum $ filter (\n -> (mod n 3 == 0) || (mod n 5 == 0)) [1..number-1]
